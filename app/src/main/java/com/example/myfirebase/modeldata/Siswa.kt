674c931492c27f193b2a9797755f9d1dae062af2
IndraHfid <indrahafidsaputra@gmail.com>
2026-01-02 15:47:22 +0700

    menambahkan state UI siswa

--- app/src/main/java/com/example/myfirebase/modeldata/Siswa.kt
+++ app/src/main/java/com/example/myfirebase/modeldata/Siswa.kt
@@ -20,3 +20,13 @@ fun DetailSiswa.toDataSiswa(): Siswa = Siswa(id, nama, alamat, telpon)
 // Fungsi ekstensi untuk mengubah model data Siswa menjadi DetailSiswa
 fun Siswa.toDetailSiswa(): DetailSiswa = DetailSiswa(id, nama, alamat, telpon)
 
+data class UIStateSiswa(
+    val detailSiswa: DetailSiswa = DetailSiswa(),
+    val isEntryValid: Boolean = false
+)
+
+// Fungsi ekstensi untuk memetakan Siswa langsung ke UIStateSiswa
+fun Siswa.toUiStateSiswa(isEntryValid: Boolean = false): UIStateSiswa = UIStateSiswa(
+    detailSiswa = this.toDetailSiswa(),
+    isEntryValid = isEntryValid
+)
