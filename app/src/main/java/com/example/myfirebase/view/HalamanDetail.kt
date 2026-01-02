34c06ea40a9f290fe39b3204fd6629bad146c703
IndraHfid <indrahafidsaputra@gmail.com>
2026-01-02 20:35:48 +0700

    tambah tampilan Detail Siswa

---
+++ app/src/main/java/com/example/myfirebase/view/HalamanDetail.kt
@@ -0,0 +1,50 @@
+package com.example.myfirebase.view
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.myfirebase.R
+import com.example.myfirebase.view.route.DestinasiDetail
+import com.example.myfirebase.viewmodel.DetailViewModel
+import com.example.myfirebase.viewmodel.PenyediaViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DetailSiswaScreen(
+    idSiswa: Long,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: DetailViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val scrollBehavior = TopAppBarDefaults.exitUntilCollapsedScrollBehavior()
+
+
+    Scaffold(
+        modifier = modifier,
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiDetail.titleRes),
+                canNavigateBack = true,
+                navigateUp = navigateBack,
+                scrollBehavior = scrollBehavior
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxWidth()
+                .padding(dimensionResource(id = R.dimen.padding_medium)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_large))
+        ) {
+            Text(text = viewModel.detail.nama, style = MaterialTheme.typography.titleLarge)
+            Text(text = viewModel.detail.telpon, style = MaterialTheme.typography.bodyLarge)
+            Text(text = viewModel.detail.alamat, style = MaterialTheme.typography.bodyLarge)
+        }
+    }
+}
+
