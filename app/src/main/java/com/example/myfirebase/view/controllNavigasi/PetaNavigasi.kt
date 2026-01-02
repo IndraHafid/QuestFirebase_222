85ff9bfcfe3e17cebf56a4221d588ce398f59808
IndraHfid <indrahafidsaputra@gmail.com>
2026-01-02 20:57:32 +0700

    tambah navigasi ke halaman detail dan edit siswa

--- app/src/main/java/com/example/myfirebase/view/controllNavigasi/PetaNavigasi.kt
+++ app/src/main/java/com/example/myfirebase/view/controllNavigasi/PetaNavigasi.kt
@@ -3,6 +3,7 @@ package com.example.myfirebase.view.controllNavigasi
 import com.example.myfirebase.view.route.DestinasiEntry
 import com.example.myfirebase.view.route.DestinasiHome
 import com.example.myfirebase.view.route.DestinasiEdit
+import com.example.myfirebase.view.route.DestinasiDetail
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
@@ -31,11 +32,28 @@ fun HostNavigasi(
         composable(DestinasiHome.route) {
             HomeScreen(
                 navigateToItemEntry = { navController.navigate(DestinasiEntry.route) },
-                navigateToItemUpdate = { id -> navController.navigate("${DestinasiEdit.route}/$id") }
+                navigateToItemEdit = { id -> navController.navigate("${DestinasiEdit.route}/$id") },
+                navigateToItemDetail = { id -> navController.navigate("${DestinasiDetail.route}/$id") }
             )
         }
         composable(DestinasiEntry.route) {
             EntrySiswaScreen(
+                navigateBack = { navController.navigate(DestinasiHome.route) }
+            )
+        }
+        composable(DestinasiEdit.routeWithArgs) { backStackEntry ->
+            val idArg = backStackEntry.arguments?.getString(DestinasiEdit.itemIdArg)
+            val id = idArg?.toLongOrNull() ?: 0L
+            com.example.myfirebase.view.EditSiswaScreen(
+                idSiswa = id,
+                navigateBack = { navController.navigate(DestinasiHome.route) }
+            )
+        }
+        composable(DestinasiDetail.routeWithArgs) { backStackEntry ->
+            val idArg = backStackEntry.arguments?.getString(DestinasiDetail.itemIdArg)
+            val id = idArg?.toLongOrNull() ?: 0L
+            com.example.myfirebase.view.DetailSiswaScreen(
+                idSiswa = id,
                 navigateBack = { navController.navigate(DestinasiHome.route) }
             )
         }
