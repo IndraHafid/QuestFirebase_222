d2b424327632cfdd1e8153a1d16ad9e75c78aab3
IndraHfid <indrahafidsaputra@gmail.com>
2026-01-02 21:01:00 +0700

    menambahkan layar Edit Siswa dengan fungsi edit dan simpan data

---
+++ app/src/main/java/com/example/myfirebase/view/HalamanEdit.kt
@@ -0,0 +1,70 @@
+package com.example.myfirebase.view
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.stringResource
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.myfirebase.R
+import com.example.myfirebase.view.route.DestinasiEdit
+import com.example.myfirebase.viewmodel.EditViewModel
+import com.example.myfirebase.viewmodel.PenyediaViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun EditSiswaScreen(
+    idSiswa: Long,
+    navigateBack: () -> Unit,
+    modifier: Modifier = Modifier,
+    viewModel: EditViewModel = viewModel(factory = PenyediaViewModel.Factory)
+) {
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+
+    // ketika layar tampil, load data
+    LaunchedEffect(key1 = idSiswa) {
+        viewModel.loadSiswaById(idSiswa)
+    }
+
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            SiswaTopAppBar(
+                title = stringResource(DestinasiEdit.titleRes),
+                canNavigateBack = true,
+                navigateUp = navigateBack,
+                scrollBehavior = scrollBehavior
+            )
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+                .padding(dimensionResource(id = R.dimen.padding_medium)),
+            verticalArrangement = Arrangement.spacedBy(dimensionResource(id = R.dimen.padding_large))
+        ) {
+            FormTambahSiswa(
+                detailSiswa = viewModel.uiStateSiswa.detailSiswa,
+                onValueChange = viewModel::updateUiState,
+                modifier = Modifier.fillMaxWidth()
+            )
+
+            Button(
+                onClick = {
+                    viewModel.saveSiswa { navigateBack() }
+                },
+                enabled = viewModel.uiStateSiswa.isEntryValid,
+                shape = MaterialTheme.shapes.small,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Text(stringResource(R.string.btn_submit))
+            }
+        }
+    }
+}
